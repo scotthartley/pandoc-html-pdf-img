ac1eb91a78e04530fd6dcf4dee8a0aeff3bb29c6
Scott Hartley <scott.hartley@miamioh.edu>
2015-09-17 17:25:29 -0400

    First working version

---
+++ README.md
@@ -0,0 +1,7 @@
+# pandoc-html-pdf-img: A pandoc filter for forcing pdf images to be inserted in html as images
+
+pandoc-html-pdf-img is a simple [pandoc][] filter written in python 3. It forces pdf images to be
+inserted into html as images, not embedded. This is handy when using pandoc with [Marked][].
+
+[pandoc]: http://pandoc.org
+[Marked]: http://marked2app.com
---
+++ pandoc-html-pdf-img.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+"""Pandoc filter to force the <img> tag for pdf images in html.
+
+Useful for integrating pandoc with Marked.
+
+"""
+
+from pandocfilters import toJSONFilter, RawInline, Str
+import re
+
+def pdf_img(key, val, fmt, meta):
+    if key == 'Image' and fmt == 'html':
+        caption, target = val
+
+        precap = "<figure><img src=" + target[0] + " /><figcaption>"
+        postcap = "</figcaption></figure>"
+
+        return [RawInline(fmt, precap)] + caption + [RawInline(fmt, postcap)]
+
+if __name__ == '__main__':
+    toJSONFilter(pdf_img)
